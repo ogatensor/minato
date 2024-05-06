0704d96c2089fd68b35a4fac54ffc28a2e1fe8d6
ogatensor <drew.tensor@gmail.com>
2024-05-06 17:25:29 -0400

    Remove target directory from repository

--- minato/target/debug/deps/minato-c1f0b6fe2314e3fa.d
+++
@@ -1,7 +0,0 @@
-/Users/andrewowens/Library/Mobile Documents/iCloud~md~obsidian/Documents/OFA/minato/target/debug/deps/minato-c1f0b6fe2314e3fa: src/main.rs src/transaction_parameters.rs src/price_calculator.rs
-
-/Users/andrewowens/Library/Mobile Documents/iCloud~md~obsidian/Documents/OFA/minato/target/debug/deps/minato-c1f0b6fe2314e3fa.d: src/main.rs src/transaction_parameters.rs src/price_calculator.rs
-
-src/main.rs:
-src/transaction_parameters.rs:
-src/price_calculator.rs:
